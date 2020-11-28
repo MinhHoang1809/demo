f60a1c0939a0af5f836158d56c54b973c4f1daef
Minh Hoang <minhhoang220676@gmail.com>
2020-11-29 01:25:39 +0700

    Updates

---
+++ precache-manifest.f80d472dc59c6f886dc6b6404fb50610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642409a77b242dc04f8d0f6d03420ed2",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "4bf6acfc581dee186610",
+    "url": "/demo/static/css/main.2cb127c3.chunk.css"
+  },
+  {
+    "revision": "f714f5cb8ff55058a839",
+    "url": "/demo/static/js/2.cad20568.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo/static/js/2.cad20568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf6acfc581dee186610",
+    "url": "/demo/static/js/main.91718b3a.chunk.js"
+  },
+  {
+    "revision": "243188d1dbc04d2f756d",
+    "url": "/demo/static/js/runtime-main.d0eab8c6.js"
+  }
+]);
